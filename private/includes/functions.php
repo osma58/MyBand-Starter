2daa24b90902c79d7a1b32bb5ee68f336af0ee52
osman <27050@ma-web.nl>
2019-05-22 12:45:10 +0200

    update..

--- private/includes/functions.php
+++ private/includes/functions.php
@@ -1,17 +1,75 @@
 <?php
 
-function dbConnect($config)
-{
+function dbConnect() {
 
-    try {
-        $dsn = 'mysql:host=' . $config['DB_HOST'] . ';dbname=' . $config['DB_NAME'];
+	$config = require __DIR__ . '/config.php';
 
-        $connection = new PDO($dsn, $config['DB_USER'], $config['DB_PASSWORD']);
+	try {
+		$dsn = 'mysql:host=' . $config['DB_HOST'] . ';dbname=' . $config['DB_NAME'];
 
-        return $connection;
+		$connection = new PDO( $dsn, $config['DB_USER'], $config['DB_PASSWORD'] );
 
-    } catch (\PDOException $e) {
-        echo 'Fout bij maken van database verbinding: ' . $e->getMessage();
-    }
+		$connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		$connection->setAttribute( PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC );
+
+		return $connection;
+
+	} catch ( \PDOException $e ) {
+		echo 'Fout bij maken van database verbinding: ' . $e->getMessage();
+	}
+
+}
+
+// Interne functie om de juiste URL vanaf localhost te bepalen
+// Voor mensen die de website in een subfolder hebben staan
+function base_url() {
+	global $CONFIG;
+
+
+	// base directory
+	$public_dir = preg_replace( "!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME'] );
+
+	// server protocol
+	$protocol = empty( $_SERVER['HTTPS'] ) ? 'http' : 'https';
+
+	// domain name
+	$domain = $_SERVER['SERVER_NAME'];
+
+	// base url
+	$base_url = preg_replace( "!^${public_dir}!", '', $CONFIG['WEBROOT'] );
+
+	// server port
+	$port      = $_SERVER['SERVER_PORT'];
+	$disp_port = ( ( $protocol === 'http' && $port === 80 ) || ( $protocol === 'https' && $port === 443 ) ) ? '' : ":$port";
+
+	// put em all together to get the complete base URL
+	$url = "${protocol}://${domain}${disp_port}${base_url}";
+
+	return $url;
+}
+
+/**
+ * Geeft de juiste URL terug voor het opgegeven path
+ * Bijvoorbeeld voor de homepage: echo url('/');
+ *
+ * @param $path
+ *
+ * @return string
+ */
+function url( $path ) {
+	return base_url() . $path;
+}
+
+/**
+ * Hier maken we de template engine aan, we geven de template engine het pad naar onze views (templates)
+ * @return \League\Plates\Engine
+ */
+function get_template_engine() {
+	global $CONFIG;
+
+	$templates_path  = $CONFIG['PRIVATE'] . '/views';
+	$template_engine = new League\Plates\Engine( $templates_path );
+
+	return $template_engine;
 
 }
--- private/includes/init.php
+++ private/includes/init.php
@@ -1,4 +1,7 @@
 <?php
+require_once $CONFIG['PRIVATE'] . '/includes/functions.php';
+require_once $CONFIG['PRIVATE'] . '/models/model.php';
+
 /**
  * Onderstaande code zorgt ervoor dat als we in onze code een controller willen gebruiken bijv:
  *
@@ -19,4 +22,35 @@ spl_autoload_register( function ( $className ) {
 	}
 
 	require_once $fileName;
-});
+} );
+
+
+/**
+ * Deze function zorgt ervoor dat de template engine code ingeladen wordt vanaf de juiste folder
+ */
+spl_autoload_register( function ( $class ) {
+	global $CONFIG;
+
+	// Template engine namespace prefix
+	$prefix = 'League\\';
+
+	// base directory for the namespace prefix
+	$base_dir = $CONFIG['PRIVATE']. '/src/';
+
+	// does the class use the namespace prefix?
+	$len = strlen( $prefix );
+	if ( strncmp( $prefix, $class, $len ) !== 0 ) {
+		// no, move to the next registered autoloader
+		return;
+	}
+
+	// replace the namespace prefix with the base directory, replace namespace
+	// separators with directory separators in the relative class name, append
+	// with .php
+	$file = $base_dir . str_replace( '\\', '/', $class ) . '.php';
+
+	// if the file exists, require it
+	if ( file_exists( $file ) ) {
+		require $file;
+	}
+} );
